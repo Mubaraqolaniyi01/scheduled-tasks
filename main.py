913824527960a1803c8e1e4af9f469c14272fa40
Mubaraqolaniyi01 <olaniyimubaraq@gmail.com>
2026-03-30 19:36:48 +0100

    main.py

--- main.py
+++ main.py
@@ -5,34 +5,27 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
-import os
-
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+now = dt.datetime.now()
+today_tuple = (now.month, now.day)
+ # Limited to 1 person
+data = pd.read_csv("birthdays.csv")
+birthday_dict = {(data_row.month, data_row.day):data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthday_dict:
+    celebrant = birthday_dict[today_tuple]
+    file_path = f"letter_{random.randint(1, 3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("[NAME]", celebrant["name"])  # To avoid not replacing the [NAME], we redefine the variable
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with s.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            to_addrs=celebrant["email"],
+            msg=f"Subject: Birthday Heralds\n\n{contents}"
         )
+        print(f"Email sent successfully to {celebrant["name"]}")
