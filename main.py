ae1acd204edec7616282d3228914da0b49a7f964
Mubaraqolaniyi01 <olaniyimubaraq@gmail.com>
2026-04-08 20:24:13 +0100

    main.py

--- main.py
+++ main.py
@@ -28,3 +28,40 @@ if today_tuple in birthday_dict:
             msg=f"Subject: Birthday Heralds\n\n{contents}"
         )
         print(f"Email sent successfully to {celebrant["name"]}")
+#_________________________________________________________
+import requests
+from twilio.rest import Client
+OPEN_WM = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID)")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+parameters={
+    "lat": 4.771490,
+    "lon": 7.014350,
+    "appid": API_KEY,
+    "cnt": 4,
+}
+response = requests.get(url= OPEN_WM, params= parameters)
+response.raise_for_status()
+weather_data = response.json()
+shall_rain = False
+
+for hour_data in weather_data ["list"]:
+    weather_id = hour_data["weather"][0]["id"]
+    if int(weather_id) < 700:
+        shall_rain = True
+if shall_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        body="It's going to rain today. Remember to bring an ☔",
+        to="whatsapp:+447424957521"
+    )
+    print(message.status)
+
+
+
+
+
+
